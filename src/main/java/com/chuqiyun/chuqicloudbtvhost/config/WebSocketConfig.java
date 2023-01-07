2be60a777dd4a273731d7b29f5ebaa56220c67d2
mryunqi <434658198@qq.com>
2023-01-07 20:45:22 +0800

    新增websocket服务端

---
+++ src/main/java/com/chuqiyun/chuqicloudbtvhost/config/WebSocketConfig.java
@@ -0,0 +1,17 @@
+package com.chuqiyun.chuqicloudbtvhost.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @author mryunqi
+ * @date 2023/1/7
+ */
+@Configuration
+public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
