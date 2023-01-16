b7abccc3e0f46740d68ac8cfc7b29e3acadea82a
LAPTOP-E2DBMP27\Lenovo <2367645814@qq.com>
2023-01-16 19:58:18 +0800

    增加主机列表界面

--- public/static/js/fn-hs.js
+++ public/static/js/fn-hs.js
@@ -72,7 +72,7 @@ $.notify({
     onClosed: null,
     icon_type: 'class',
     template: '<div data-notify="container" class="col-xs-11 col-sm-3 alert alert-{0}" role="alert">' +
-    	'<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+    	// '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
     	'<span data-notify="icon"></span> ' +
     	'<span data-notify="title">{1}</span> ' +
     	'<span data-notify="message">{2}</span>' +
--- src/main/java/com/chuqiyun/btvhost/controller/admin/AdminHome.java
+++ src/main/java/com/chuqiyun/btvhost/controller/admin/AdminHome.java
@@ -22,8 +22,19 @@ public class AdminHome {
     }
     @AdminLoginCheck
     @RequestMapping(value = "/admin/vhostList")
-    public String hostlist(){
+    public String hostlist(Model model){
+        model.addAttribute("title", "控制台");
+        model.addAttribute("page","adminHome");
+        model.addAttribute("page_tab","adminHome");
         return "/admin/hostlist";
     }
 
+    @AdminLoginCheck
+    @RequestMapping(value = "/admin/addhost")
+    public String addhost(Model model){
+        model.addAttribute("title", "控制台");
+        model.addAttribute("page","adminHome");
+        model.addAttribute("page_tab","adminHome");
+        return "/admin/addhost";
+    }
 }
