8220b087aeef5269b33597736b0fb3b7be2fa957
LAPTOP-E2DBMP27\Lenovo <2367645814@qq.com>
2023-01-20 12:28:38 +0800

    再增加help页面

--- src/main/java/com/chuqiyun/btvhost/controller/admin/AdminHome.java
+++ src/main/java/com/chuqiyun/btvhost/controller/admin/AdminHome.java
@@ -23,18 +23,26 @@ public class AdminHome {
     @AdminLoginCheck
     @RequestMapping(value = "/admin/vhostList")
     public String hostlist(Model model){
-        model.addAttribute("title", "控制台");
-        model.addAttribute("page","adminHome");
-        model.addAttribute("page_tab","adminHome");
+        model.addAttribute("title", "主机列表");
+        model.addAttribute("page","vhost");
+        model.addAttribute("page_tab","vhostList");
         return "/admin/hostlist";
     }
 
     @AdminLoginCheck
     @RequestMapping(value = "/admin/addVhost")
     public String addhost(Model model){
-        model.addAttribute("title", "控制台");
-        model.addAttribute("page","adminHome");
-        model.addAttribute("page_tab","adminHome");
+        model.addAttribute("title", "添加主机");
+        model.addAttribute("page","vhost");
+        model.addAttribute("page_tab","addVhost");
         return "/admin/addhost";
+    }
+    @AdminLoginCheck
+    @RequestMapping(value = "/admin/help")
+    public String help(Model model){
+        model.addAttribute("title", "教程与帮助");
+        model.addAttribute("page","command");
+        model.addAttribute("page_tab","help");
+        return "/admin/help";
     }
 }
